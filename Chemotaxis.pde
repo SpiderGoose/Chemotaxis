9684e8989d96c61229427217f1a171b9003affb0
unknown <rijaime_jr@697-334-Smaug.sf.k12.internal>
2015-09-17 11:35:45 -0700

    Done but i can add more

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,21 +1,25 @@
  //declare bacteria variables here
 
  Bacteria  [] clique;
+ color purple = color(61, 0, 153,150);
+ color yellow = color(207, 207, 0,150);
+ color green = color(0, 138, 46,150);
+ color red = color(147, 0, 0,150);
+
  void setup()   
  {     
  	background(150);
  	//initialize bacteria variables here   
- 	size(1000,1000);
- 	clique = new Bacteria[800];
+ 	size(800,800);
+ 	clique = new Bacteria[400];
 
 	for(int i=0; i<clique.length; i++)
 
   {
 
-
-
     clique[i]= new Bacteria();
  }  
+
 
  	
  }   
@@ -42,21 +46,95 @@
  class Bacteria    
  {     
  	int myX, myY;
+
+ 	double cRandom = Math.random();
+
+ 	double bRandom = Math.random();
  Bacteria()
  	{
- 		myX = 250;
- 		myY = 250;
+ 		
+ 		myX = (int)(Math.random()*400)+200;
+ 		myY = (int)(Math.random()*400)+200;
 
  	}
 
 void  move()
 {
-	myX = ((int)(Math.random()*5) -2) + myX;
-	myY = ((int)(Math.random()*5)-2) + myY;
+
+if(myX<mouseX)
+	myX = ((int)(Math.random()*5) -1) + myX;
+else
+	myX = ((int)(Math.random()*5) -3) + myX;
+
+if(myY<mouseY)
+	myY = ((int)(Math.random()*5) -1) + myY;
+else
+	myY = ((int)(Math.random()*5) -3) + myY;
+
+
+
+// reset
+if(myX == mouseX && myY==mouseY)
+{
+
+if(bRandom < .25)
+{
+	myX = (int)(Math.random())-100;
+	myY = (int)(Math.random()*800);
+ 		
+
+}
+	
+else if(bRandom<.5)
+{
+	myX = (int)(Math.random())+900;
+	myY = (int)(Math.random()*800);
+ 		
+
+}
+	
+else if(bRandom <.75)
+{
+	
+ 		myY = (int)(Math.random())-100;
+ 		myX = (int)(Math.random()*800);
+
+}
+	
+else 
+{
+	
+ 		myY = (int)(Math.random())+900;
+ 		myX = (int)(Math.random()*800);
+
+}
+	
+
+}
+
+
+
+	//myX = ((int)(Math.random()*5) -2) + myX;
+	//myY = ((int)(Math.random()*5)-2) + myY;
  }    
  void show()
  {
- 	fill(150,0,150,150);
+
+
+
+//color stuff
+
+if(cRandom < .25)
+	fill(purple);
+else if(cRandom<.5)
+	fill(red);
+else if(cRandom <.75)
+	fill(green);
+else 
+	fill(yellow);
+
+
+ //	fill(150,0,150,150);
  	ellipse(myX,myY,10,10);
  	noStroke();
  	fill(0,150,150,50);
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -17,28 +17,32 @@ public class Chemotaxis extends PApplet {
  //declare bacteria variables here
 
  Bacteria  [] clique;
+ int purple = color(61, 0, 153,150);
+ int yellow = color(207, 207, 0,150);
+ int green = color(0, 138, 46,150);
+ int red = color(147, 0, 0,150);
+
  public void setup()   
  {     
  	background(150);
  	//initialize bacteria variables here   
- 	size(1000,1000);
- 	clique = new Bacteria[800];
+ 	size(800,800);
+ 	clique = new Bacteria[400];
 
 	for(int i=0; i<clique.length; i++)
 
   {
 
-
-
     clique[i]= new Bacteria();
  }  
+
 
  	
  }   
  public void draw()   
  {    
 
- 	background(150);
+ 	//background(150);
 
  	for(int i=0; i<clique.length; i++)
 
@@ -58,21 +62,95 @@ public class Chemotaxis extends PApplet {
  class Bacteria    
  {     
  	int myX, myY;
+
+ 	double cRandom = Math.random();
+
+ 	double bRandom = Math.random();
  Bacteria()
  	{
- 		myX = 250;
- 		myY = 250;
+ 		
+ 		myX = (int)(Math.random()*400)+200;
+ 		myY = (int)(Math.random()*400)+200;
 
  	}
 
 public void  move()
 {
-	myX = ((int)(Math.random()*5) -2) + myX;
-	myY = ((int)(Math.random()*5)-2) + myY;
+
+if(myX<mouseX)
+	myX = ((int)(Math.random()*5) -1) + myX;
+else
+	myX = ((int)(Math.random()*5) -3) + myX;
+
+if(myY<mouseY)
+	myY = ((int)(Math.random()*5) -1) + myY;
+else
+	myY = ((int)(Math.random()*5) -3) + myY;
+
+
+
+// reset
+if(myX == mouseX && myY==mouseY)
+{
+
+if(bRandom < .25f)
+{
+	myX = (int)(Math.random())-100;
+	myY = (int)(Math.random()*800);
+ 		
+
+}
+	
+else if(bRandom<.5f)
+{
+	myX = (int)(Math.random())+900;
+	myY = (int)(Math.random()*800);
+ 		
+
+}
+	
+else if(bRandom <.75f)
+{
+	
+ 		myY = (int)(Math.random())-100;
+ 		myX = (int)(Math.random()*800);
+
+}
+	
+else 
+{
+	
+ 		myY = (int)(Math.random())+900;
+ 		myX = (int)(Math.random()*800);
+
+}
+	
+
+}
+
+
+
+	//myX = ((int)(Math.random()*5) -2) + myX;
+	//myY = ((int)(Math.random()*5)-2) + myY;
  }    
  public void show()
  {
- 	fill(150,0,150,150);
+
+
+
+//color stuff
+
+if(cRandom < .25f)
+	fill(purple);
+else if(cRandom<.5f)
+	fill(red);
+else if(cRandom <.75f)
+	fill(green);
+else 
+	fill(yellow);
+
+
+ //	fill(150,0,150,150);
  	ellipse(myX,myY,10,10);
  	noStroke();
  	fill(0,150,150,50);
